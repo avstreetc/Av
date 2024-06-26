b5ccb8725fc863bc234a9c7d5a4eec5d1f9a85b9
avstreetc <a6660778@gmail.com>
2024-06-26 22:37:14 +0300

    гзд

--- assets/JS/port.js
+++ assets/JS/port.js
@@ -24,40 +24,41 @@ window.openModal = function(workId) {
                 <p>В этой игре я знаю только, что пешка может пошагать на две клетки вперед.</p>
                 <spline-viewer loading-anim-type="spinner-small-dark" url="https://prod.spline.design/d2t7hkwYj7zyy0vX/scene.splinecode"></spline-viewer>
                 <p>Но это не помешает расставить камеры, смоделировать такую сценку.</p>
-                <img src="/assets/IMG/workcase/Chess/a.jpg" alt="Шахматы на рендере" style="max-width: 100%; border-radius: 10px;">
+                <img src="https://res.cloudinary.com/diwgonclj/image/upload/v1719430196/a_gquof8.jpg" alt="Шахматы на рендере" style="max-width: 100%; border-radius: 10px;">
                 <p>Больше всего мне нравится расставлять свет и искать заманивающие кадры.</p>
-                <img src="/assets/IMG/workcase/Chess/b.jpg" alt="Шахматы на рендере" style="max-width: 100%; border-radius: 10px;">
+                <img src="https://res.cloudinary.com/diwgonclj/image/upload/v1719430196/b_ibk8og.jpg" alt="Шахматы на рендере" style="max-width: 100%; border-radius: 10px;">
             `;
             // Создаем 3D сцену
             break;
         case "work_nightShooting":
             modalBody.innerHTML = `
                 <h2>Ночная съемка Маши</h2>
-                <img src="/assets/IMG/workcase/nightShooting/a.jpg" alt="Обородование фото" style="max-hight: 40%; border-radius: 10px;">
+                <img src="https://res.cloudinary.com/diwgonclj/image/upload/v1719430264/a_utdzbe.jpg" alt="Обородование фото" style="max-hight: 40%; border-radius: 10px;">
                 <p>Для таких кадров нам необходимо было пройти к локации и расставить свет. Это был невероятно увлекательный процесс, который позволил раскрыть всю красоту ночного города.</p>
-                <img src="/assets/IMG/workcase/nightShooting/b.jpg" alt="Первый коллаж" style="max-width: 100%; border-radius: 10px;">
+                <img src="https://res.cloudinary.com/diwgonclj/image/upload/v1719430264/b_vbgirk.jpgg" alt="Первый коллаж" style="max-width: 100%; border-radius: 10px;">
                 <p>Несколько нарядов помогают создать разные образы, а меняя фон кажется, будто фотосессий было несколько!</p>
                 <p>Свет определенно добавляет свой шарм.</p>
-                <img src="/assets/IMG/workcase/nightShooting/d.jpg" alt="Второй коллаж" style="max-width: 100%; border-radius: 10px;">
+                <img src="https://res.cloudinary.com/diwgonclj/image/upload/v1719430264/d_kl6ann.jpg" alt="Второй коллаж" style="max-width: 100%; border-radius: 10px;">
                 <p>Это было потрясающе!</p>
             `;
             break;
         case "work_dance":
             modalBody.innerHTML = `
                 <h2>Танцуем вальс Победы вместе</h2>
-                <img src="/assets/IMG/workcase/dance/a.jpg" alt="Вальс 1" style="max-width: 100%; border-radius: 10px;">
+                <img src="https://res.cloudinary.com/diwgonclj/image/upload/v1719430314/b_oyu86n.jpg" alt="Вальс 1" style="max-width: 100%; border-radius: 10px;">
                 <p>Важное мероприятие - День Победы, вальс победы. Задача была заснять работу танцоров, попутно учавствуя в проекте.</p>
-                <img src="/assets/IMG/workcase/dance/b.jpg" alt="Вальс 2" style="max-width: 100%; border-radius: 10px;">
+                <img src="https://res.cloudinary.com/diwgonclj/image/upload/v1719430315/a_s4xgrl.jpg" alt="Вальс 2" style="max-width: 100%; border-radius: 10px;">
+                <img src="https://res.cloudinary.com/diwgonclj/image/upload/v1719430315/c_uk6grl.jpg" alt="Вальс 2" style="max-width: 100%; border-radius: 10px;">
             `;
             break;
         case "work_webSearchLearn":
             modalBody.innerHTML = `
                 <h2>Сайт обучающих курсов</h2>
-                <img src="/assets/IMG/workcase/SearchLearn/c.jpg" alt="Демонстраниция сайта" style="max-width: 100%; border-radius: 10px;">
+                <img src="https://res.cloudinary.com/diwgonclj/image/upload/v1719430374/a_cc9t2f.png" alt="Демонстраниция сайта" style="max-width: 100%; border-radius: 10px;">
                 <p>Создание сайта для обучающих курсов включало в себя разработку UX/UI дизайна, адаптивной верстки и оптимизации под поисковые системы.</p>
-                <img src="/assets/IMG/workcase/SearchLearn/b.png" alt="Отображение сайта" style="max-width: 100%; border-radius: 10px;">
+                <img src="https://res.cloudinary.com/diwgonclj/image/upload/v1719430375/c_oabpv8.jpg" alt="Отображение сайта" style="max-width: 100%; border-radius: 10px;">
                 <p>Такой сайт помогает развиваться стартаперам SearchAndLearn.</p>
-                <img src="/assets/IMG/workcase/SearchLearn/a.png" alt="Сайт" style="max-width: 100%; border-radius: 10px;">
+
             `;
             break;
         case "work_limonLogo":
@@ -70,7 +71,8 @@ window.openModal = function(workId) {
         case "work_handmadeAlbum":
             modalBody.innerHTML = `
                 <h2>Альбом ручной работы</h2>
-                <img src="/assets/IMG/workcase/handmadeAlbum/a.jpg" alt="Альбом" style="max-width: 100%; border-radius: 10px;">
+                <p>Полностью подобрать размер, порезать на равные части такие страницы - и склеить - трудное дело.</p>
+                <img src="https://res.cloudinary.com/diwgonclj/image/upload/v1719430473/a_shblvl.jpg" alt="Альбом" style="max-width: 100%; border-radius: 10px;">
                 <p>Создание альбома ручной работы включало в себя разработку дизайна страниц, подбор материалов и печать.</p>
             `;
             break;
@@ -84,23 +86,23 @@ window.openModal = function(workId) {
         case "work_mobileStudio":
             modalBody.innerHTML = `
                 <h2>Выездная студия</h2>
-                <img src="/assets/IMG/workcase/mobileStudio/a.jpg" alt="Выездная студия" style="max-width: 100%; border-radius: 10px;">
+                <img src="https://res.cloudinary.com/diwgonclj/image/upload/v1719430554/a_qycuvq.jpg" alt="Выездная студия" style="max-width: 100%; border-radius: 10px;">
                 <p>Организация и проведение фотосессий с выездной студией, включая установку света и фона.</p>
                 <p>Такую студию можно взять с собой куда угодно, где угодно. Конечно, где можно спокойно проехать на машине. Кадры с таким фоном получаются живыми!</p>
-                <img src="/assets/IMG/workcase/mobileStudio/b.jpg" alt="Фото на студии" style="max-width: 100%; border-radius: 10px;">
+                <img src="https://res.cloudinary.com/diwgonclj/image/upload/v1719430554/b_moa2h2.jpg" alt="Фото на студии" style="max-width: 100%; border-radius: 10px;">
             `;
             break;
         case "work_photoWall":
             modalBody.innerHTML = `
                 <h2>Фото на стенды</h2>
-                <img src="/assets/IMG/workcase/photoWall/a.jpg" alt="Фото на стенды" style="max-width: 100%; border-radius: 10px;">
+                <img src="https://res.cloudinary.com/diwgonclj/image/upload/v1719430592/a_xrt2r7.jpg" alt="Фото на стенды" style="max-width: 100%; border-radius: 10px;">
                 <p>Съемка и печать фотографий для размещения на стендах в учебных заведениях.</p>
             `;
             break;
         case "work_boavok":
             modalBody.innerHTML = `
                 <h2>BoAvok</h2>
-                <img src="/assets/IMG/workcase/boavok/a.png" alt="BoAvok" style="max-width: 100%; border-radius: 10px;">
+                <img src="https://res.cloudinary.com/diwgonclj/image/upload/v1719430614/a_cpwxgv.png" alt="BoAvok" style="max-width: 100%; border-radius: 10px;">
                 <p>Разработка UX/UI дизайна для мобильного приложения BoAvok, включая прототипирование и тестирование.</p>
             `;
             break;
